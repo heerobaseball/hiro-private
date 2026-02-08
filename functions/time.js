bb067d7fd10cf032ffca3dc568b7cf9a7e5a247b
hiro <heero.baseball@gmail.com>
2026-02-08 23:35:26 +0900

    サイトを更新

--- functions/time.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  // 日本時間を取得
-  const now = new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' });
-  
-  // JSON形式で返す
-  return new Response(JSON.stringify({ message: `現在の時刻は ${now} です` }), {
-    headers: { "content-type": "application/json" },
-  });
-}
